34f2be0eabfdd407cb0950b117d7950d7c28860c
RyanDelbango <ryan.delbango@gmail.com>
2020-07-08 11:32:36 -0400

    5

--- api.py
+++ api.py
@@ -9,7 +9,6 @@ from Blade import Blade
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
-@app.route("/")
 def BladeList():
     Pyra = Blade("Pyra", "https://vignette.wikia.nocookie.net/xenoblade/images/4/4e/XC2-element-fire.png/revision/latest/scale-to-width-down/36?cb=20180425030957", "https://babd.wincenworks.com/wp-content/uploads/2017/12/tumblr_p1bvw1TF4g1s755fuo4_r1_1280-2.png", "https://vignette.wikia.nocookie.net/xenoblade/images/f/fd/XC2_Aegis_Sword_icon.png/revision/latest/scale-to-width-down/40?cb=20190404044917", "Pyra is a kind-hearted, mild-mannered, soft spoken young woman who desires peace among both Blades and Drivers.")
     Mythra = Blade("Mythra", "https://vignette.wikia.nocookie.net/xenoblade/images/7/71/XC2-element-light.png/revision/latest/scale-to-width-down/36?cb=20180425030958", "https://vignette.wikia.nocookie.net/p__/images/2/2b/Mythra.png/revision/latest?cb=20180517124742&path-prefix=protagonist", "https://vignette.wikia.nocookie.net/xenoblade/images/4/4c/XC2_Aegis_Sword_II_icon.png/revision/latest/scale-to-width-down/40?cb=20200426104645", "Mythra is confident in herself, reckless, and independent. She has a short temper, a rude disposition, and is not afraid to openly express her thoughts.")
@@ -48,7 +47,7 @@ def BladeList():
 
     BladeList = [Pyra, Mythra, Dromarch, Nia, Roc, Poppi, Brighid, Aegaeon, Pandoria, Godfrey, Wulfric, Perceval, Vale, Agate, Gorg, Boreas, Dagas, Kasandra, Praxis, Theory, Perun, Kora, Azami, Ursula, Newt, Nim, Sheba, Vess, Adenine, Electra, Zenobia, Finch, Floren, Herald, Dahlia]
     y = BladeList[random.randint(0,34)]
-    return {"Blade": str(y.name), "Element": str(y.element), "Picture": str(y.picture), "Weapon": str(y.weapon), "Summary": str(y.summary)}
+    return {"Blade": strf(y.name), "Element": str(y.element), "Picture": str(y.picture), "Weapon": str(y.weapon), "Summary": str(y.summary)}
 if __name__ == "__main__":
     app.run() 
 app.run(debug=True)
